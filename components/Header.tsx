2616cfed71d08b7b80a8d1678e29fa3cd0960247
eglereact <eglereactjs@gmail.com>
2022-07-04 18:03:13 +0300

    nextauth with reddit

--- components/Header.tsx
+++ components/Header.tsx
@@ -11,8 +11,11 @@ import {
   PlusCircleIcon,
   MenuIcon,
 } from "@heroicons/react/outline";
+import { signIn, signOut, useSession } from "next-auth/react";
 
 function Header() {
+  const { data: session } = useSession();
+
   return (
     <div className="sticky top-0 z-50 flex bg-white px-4 py-2 shadow-sm">
       <div className="relative h-10 w-20 flex-shrink-0 cursor-pointer">
@@ -51,12 +54,31 @@ function Header() {
       </div>
 
       {/* Sign In / Sign Out Button */}
-      <div className="hidden lg:flex items-center space-x-2 border border-gray-100 p-2 cursor-pointer">
-        <div className="relative w-5 h-5 flex-shrink-0">
-          <Image src="/logo.png" layout="fill" objectFit="contain" />
+      {session ? (
+        <div
+          onClick={() => signOut()}
+          className="hidden lg:flex items-center space-x-2 border border-gray-100 px-2 cursor-pointer"
+        >
+          <div className="relative w-5 h-5 flex-shrink-0">
+            <Image src="/logo.png" layout="fill" objectFit="contain" />
+          </div>
+          <div className="flex-1 text-xs">
+            <p className="truncate">{session?.user?.name}</p>
+            <p className="text-gray-400 text-xs">12 karma</p>
+          </div>
+          <ChevronDownIcon className="h-5 flex-shrink-0 flex-gray-400" />
         </div>
-        <p className="text-gray-400">Sign In</p>
-      </div>
+      ) : (
+        <div
+          onClick={() => signIn()}
+          className="hidden lg:flex items-center space-x-2 border border-gray-100 p-2 cursor-pointer"
+        >
+          <div className="relative w-5 h-5 flex-shrink-0">
+            <Image src="/logo.png" layout="fill" objectFit="contain" />
+          </div>
+          <p className="text-gray-400">Sign In</p>
+        </div>
+      )}
     </div>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -9,8 +9,6 @@ const Home: NextPage = () => {
         <title>Tidder - Reddit Clone</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <Header />
     </div>
   );
 };

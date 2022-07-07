82d2a25fc49768fffd5815d5169dd18f7cf885da
eglereact <eglereactjs@gmail.com>
2022-07-07 16:25:50 +0300

    stepzen impl

---
+++ apollo-client.js
@@ -0,0 +1,9 @@
+import { ApolloClient, InMemoryCache } from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: "https://palmerton.stepzen.net/api/coiled-bunny/__graphql",
+  headers: { Authorization: `Apikey ${process.env.NEXT_PUBLIC_STEPZEN_KEY}` },
+  cache: new InMemoryCache(),
+});
+
+export default client;
